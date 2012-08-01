870d57c70e57b705206803b143d5ef2edba8e11d
Brendon Scheinman <bscheinman@gmail.com>
2012-08-01 07:37:31 -0500

    committing minor changes to classifier before switching over to laptop

--- skrim/classifier.py
+++ skrim/classifier.py
@@ -5,9 +5,6 @@
 
 import numpy as np
 
-sigmoid = lambda x: 1 / (1 + exp(-x))
-
-
 class GradientDescent(object):
 
     def __init__(x, y, cost_function, alpha, max_iter = 1000, min_change = None):
@@ -25,22 +22,28 @@ class GradientDescent(object):
             min_change: if this is specified, descent will stop whenever
                 the cost decrease is below this value
         """
-        self.x = x
         self.y = y
         self.cost_function = cost_function
         self.alpha = alpha
         self.max_iter = max_iter
         self.min_change = min_change
 
+        # Pad x with leading zeros to act as x_0
+        m = x.shape[0]
+        #x = np.append([[1] for i in xrange(x.shape[0])], x, 1)
+        x = np.append(np.ones(m).reshape([m, 1]), x, 1)
+        self.x = x
+
         cost_history = []
         theta = np.zeros([x.shape[1]])
         for i in xrange(max_iter):
             cost, gradients = cost_function(x, y, theta)
             cost_history.append(cost)
-            if cost == 0 or min_change and cost_history and cost_history[-1] - cost <= min_change:
+            if cost == 0 or min_change and len(cost_history) > 1 and cost_history[-2] - cost <= min_change:
                 break
             
             theta = theta - alpha * gradients
 
         self.cost_history = cost_history
+        self.cost = cost_history[-1]
         self.theta = theta
