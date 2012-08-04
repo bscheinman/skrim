edd14382a5b865efc5c649862a17396da7aa24d8
Brendon Scheinman <brendon@caseinsensitive.org>
2012-08-04 17:13:12 -0500

    cleaned up cost functions more and added neural net cost function, although all of these changes are untested

--- skrim/cost_functions.py
+++ skrim/cost_functions.py
@@ -2,6 +2,8 @@ import numpy as np
 from math import exp, log
 
 sigmoid = lambda x: 1 / (1 + exp(-x))
+sigmoid_curry = np.vectorize(sigmoid)
+
 
 
 class CostFunction(object):
@@ -21,9 +23,10 @@ class RegressionCost(CostFunction):
 
     def __init__(self, row_value_getter, row_cost_getter, regular_coeff=0):
         """
-            reg_type: the type of regression to run.  options are:
-                linear
-                logistic
+            row_value_getter: a function that calculates the predicted value of an observation
+                given the feature values and theta
+            row_cost_getter: a function that calculates the cost of an observation given the
+                predicted value and the observed value
             regular_coeff: the coefficient to use for regularization.  Usually referred to as lambda,
                 but for obvious reasons can't be named that.  If set to 0, regularization won't be used.
         """
@@ -53,7 +56,7 @@ class RegressionCost(CostFunction):
         # TODO: vectorize this
         for i in xrange(m):
             row = x[i,:]
-            row_value = self.get_row_value(row)
+            row_value = self.get_row_value(row, theta)
             cost += self.get_cost(row_value, y[i])
             gradients += row * (row_value - y[i])
 
@@ -70,7 +73,7 @@ class RegressionCost(CostFunction):
 class LinearRegression(RegressionCost):
     def __init__(self, regular_coeff = 0):
         super(LinearRegression, self).__init__(
-            (lambda row: np.dot(row, theta)),
+            (lambda row, theta: np.dot(row, theta)),
             (lambda predicted, actual: ((predicted - actual) ** 2) * 2),
             regular_coeff)
 
@@ -78,6 +81,49 @@ class LinearRegression(RegressionCost):
 class LogisticRegression(RegressionCost):
     def __init__(self, regular_coeff = 0):
         super(LogisticRegression, self).__init__(
-            (lambda row: sigmodid(np.dot(row, theta))),
+            (lambda row, theta: sigmoid(np.dot(row, theta))),
             (lambda predicted, actual: -log(predicted if actual else (1 - predicted))),
             regular_coeff)
+
+
+class NeuralNetCost(CostFunction):
+
+    # TODO: refactor to work with an arbitrary number of levels
+    def __init__(self, n_input, n_hidden, n_labels, regular_coeff = 0):
+        """
+            n_input: the number of input features
+            n_hidden: the number of nodes in the (first) hidden layer of the neural net
+            n_labels: the number of possible output labels
+            regular_coeff: the value to use for lambda (optional)
+        """
+        self.n_input = n_input
+        self.n_hidden = n_hidden
+        self.n_labels = n_labels
+        self.regular_coeff = regular_coeff
+
+
+    def calculate(self, x, y, theta):
+
+        theta_1 = theta[0 : n_hidden * (n_input + 1) - 1].reshape([n_hidden, n_input + 1])
+        theta_2 = theta[n_hidden * (n_input + 1) : theta.size - 1]\
+            .reshape([n_labels, n_hidden + 1])
+
+        m = x.shape[0]
+        x = np.append(np.ones(m, 1), x, 1)
+        cost = 0
+
+        for i in xrange(m):
+            a1 = x[i,:]
+            a2 = sigmoid_curry(np.dot(a1, theta_1.T))
+            a2_input = np.append(np.ones(n_hidden, 1), a2, 1)
+            # a3 represents the predicted values
+            a3 = sigmoid_curry(np.dot(a2_input, theta_2.T))
+            actual = np.zeros(1, num_labels);
+            actual[y[i]] = 1;
+            cost -= sum(actual * log(a3) + (1 - actual) * log(1 - a3))
+
+        if self.regular_coeff:
+            cost += (regular_coeff / 2) * (sum(theta_1[:,1:] ** 2) + sum(theta_2[:,1:] ** 2))
+
+        cost /= m
+        return cost
