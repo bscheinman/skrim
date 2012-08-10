e94cee72f92ff7726ae0d2aec264dbb9cf300f0f
Brendon Scheinman <bscheinman@gmail.com>
2012-08-10 18:15:44 -0500

    added RangeNormalizer, which uses the min and max of each feature to transform features to a [0,1] range

--- skrim/normalize.py
+++ skrim/normalize.py
@@ -1,6 +1,7 @@
 from abc import abstractmethod
 import numpy as np
 
+
 class Normalizer(object):
     """
 
@@ -13,7 +14,6 @@ class Normalizer(object):
             this method must be called before normalizing any data
         """
 
-
     @abstractmethod
     def normalize(self, x):
         """
@@ -21,14 +21,11 @@ class Normalizer(object):
             returns input data normalized to the scale of the basis data
         """
 
-
     @abstractmethod
     def reset(self):
         """
             resets the normalizer to be in its initial state
         """
-
-
 
 
 class StandardNormalizer(Normalizer):
@@ -41,21 +38,49 @@ class StandardNormalizer(Normalizer):
     def __init__(self):
         self.reset()
 
-
     def set_basis(self, x):
         self.means = np.mean(x, 0)
         self.stds = np.std(x, 0)
 
-
     def normalize(self, x):
         n = x.shape[1]
         if n != self.means.shape[0]:
-            raise ValueError('this normalizer is initialized to take input with %s features but you provided input with %s features'
+            raise ValueError(
+                'this normalizer is initialized to take input with %s features but you provided input with %s features'
                 % (str(self.means.shape[0])), x.shape[1])
 
         return (x - self.means) / self.stds
 
-
     def reset(self):
         self.means = np.array([])
         self.stds = np.array([])
+
+
+class RangeNormalizer(Normalizer):
+
+    """
+        Uses the total range of each feature to normalize values.
+        All values are normalized linearly into [0, 1], where the minimum
+        value will be normalized to 0 and the maximum value will be
+        normalized to 1.
+    """
+
+    def __init__(self):
+        self.reset()
+
+    def set_basis(self, x):
+        self.mins = np.vectorize(float)(np.min(x, 0))
+        self.maxs = np.vectorize(float)(np.max(x, 0))
+
+    def normalize(self, x):
+        n = x.shape[1]
+        if n != self.mins.shape[0]:
+            raise ValueError(
+                'this normalizer is initialized to take input with %s features but you provided input with %s features'
+                % (str(self.mins.shape[0])), x.shape[1])
+
+        return (x - self.mins) / (self.maxs - self.mins)
+
+    def reset(self):
+        self.mins = np.array([])
+        self.maxs = np.array([])
